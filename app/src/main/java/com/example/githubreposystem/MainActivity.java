6a369e298198125f15c569866e2faf09fc9a41e3
Jagadeesh <vdeesanjaga@gmail.com>
2020-10-23 14:18:42 +0530

    Showing dialog while receiving FCM

--- app/src/main/java/com/example/githubreposystem/MainActivity.java
+++ app/src/main/java/com/example/githubreposystem/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.githubreposystem;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.githubreposystem.ui.dashboard.DashboardFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -18,6 +27,7 @@ import androidx.viewpager.widget.ViewPager;
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
+    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,new IntentFilter("custom-action-local-broadcast"));
     }
 
     @Override
@@ -47,5 +57,37 @@ public class MainActivity extends AppCompatActivity {
         }else{
             super.onBackPressed();
         }
+    }
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String title = intent.getStringExtra("title");
+            final String message = intent.getStringExtra("name");
+            Log.d("receiver", "Got message: " + message);
+            alertDialog(message);
+
+        }
+    };
+
+    private void alertDialog(final String message)
+    {
+        builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("You Received a FCM Message ")
+                .setCancelable(false)
+                .setPositiveButton("SHOW", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(getApplicationContext(),message,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.setTitle("FCM Message");
+        alert.show();
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mMessageReceiver);
     }
 }
--- app/src/main/java/com/example/githubreposystem/MyService.java
+++ app/src/main/java/com/example/githubreposystem/MyService.java
@@ -5,12 +5,14 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -61,6 +63,10 @@ public class MyService extends FirebaseMessagingService {
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+            //Intent intent = new Intent("custom-event-name");
+            // You can also include some extra data.
+           // intent.putExtra("message", "This is my message!");
+            //LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
 
         }
 
@@ -70,8 +76,15 @@ public class MyService extends FirebaseMessagingService {
         }
 
         // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
+        // message, here is where that should be initiated. See
+        //  sendNotification method below.
+        sendNotification(remoteMessage.getNotification().getBody());
+        Intent intent = new Intent("custom-action-local-broadcast");
+        intent.putExtra("title",remoteMessage.getNotification().getTitle());
+        intent.putExtra("name",remoteMessage.getNotification().getBody());
+        LocalBroadcastManager.getInstance(MyService.this).sendBroadcast(intent);
     }
+
     // [END receive_message]
 
 
